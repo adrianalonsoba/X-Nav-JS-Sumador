941aaad516db0b14e98134ab8d445a3d0fffbe5b
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-02 19:10:56 +0100

    terminada calculadora aleatoria

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function changer (id, newValue) {
 }
 
 function sum(){
-
 	var ops=document.getElementById('op').innerHTML.split('+');
 	var result= parseInt(ops[0])+parseInt(ops[1]);
 	changer('res','='+result);	
+}
 
+function random(){
+	var op1= Math.floor((Math.random() * 10) + 1);
+	var op2= Math.floor((Math.random() * 10) + 1);
+	changer('op',op1+'+'+op2);
 }

907e1ec60bd554c9c5c650a6f17bf4931fa05565
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-02 18:45:22 +0100

    calculadora simple acabada

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-movie={title:'1234',
-       year:'9999'}
-for(i in movie){alert(movie[i])}
+function changer (id, newValue) {
+	var element = document.getElementById(id);
+	element.innerHTML = newValue;
+}
 
+function sum(){
+
+	var ops=document.getElementById('op').innerHTML.split('+');
+	var result= parseInt(ops[0])+parseInt(ops[1]);
+	changer('res','='+result);	
+
+}
